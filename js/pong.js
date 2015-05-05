90e9cab60fe132f669664e95aa4b5d48696d26d8
Vince Maiuri <RookY2K@gmail.com>
2015-05-05 07:04:16 -0500

    Added skeleton for connector closure

--- js/pong.js
+++ js/pong.js
@@ -1,6 +1,61 @@
 /**
  * Created by Vince Maiuri on 5/4/2015.
  */
+var connector = function () {
+    "use strict";
+    var onOpen, onMessage, onError, onClose, sendMessage,
+        open, channel, socket, playerName;
 
+    open = function (token, name) {
+        playerName = name;
+        //noinspection JSUnresolvedFunction
+        channel = new goog.appengine.Channel(token);
+        socket = channel.open();
+        socket.onopen = onOpen;
+        socket.onclose = onClose;
+        socket.onerror = onError;
+        socket.onmessage = onMessage;
+    };
 
-$
+    onOpen = function () {
+        //TODO finish stub
+        return null;
+    };
+
+    onMessage = function (message) {
+        //TODO finish stub
+        return message;
+    };
+
+    onError = function () {
+        //TODO finish stub
+        return null;
+    };
+
+    onClose = function () {
+        //TODO finish stub
+        return null;
+    };
+
+    sendMessage = function (path, data) {
+        data = data || {};
+
+        $.post(path,  data,  "json");
+    }
+};
+
+$(document).ready(function () {
+    "use strict";
+    var userName = window.prompt("User name: ");
+
+    //noinspection JSUnusedGlobalSymbols
+    $.ajax({
+        url: "/connect",
+        data: {"userName": userName},
+        dataType: "json",
+        success: function (data) {
+            //TODO fill in stub
+            return data;
+        }
+    });
+});
